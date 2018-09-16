d5cc3673c9b6f97d160899c0f4ef4b80ea09c2f2
asus <2548942334@qq.com>
2018-09-16 15:22:29 +0800

    修改标题

---
+++ moving/moving.js
@@ -0,0 +1,38 @@
+function getstyle(obj,attr) {
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];
+    }else{
+        return window.getComputedStyle(obj,null)[attr];
+    }
+}
+function startmove(obj,json,fn) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        var bstop=true;
+        for(var attr in json){
+            var icur =0;
+            if(attr=="opacity"){
+                icur=parseInt(parseFloat(getstyle(obj,attr))*100);
+            }else{
+                icur=parseInt(getstyle(obj,attr));
+            }
+            var iSpeed = (json[attr] - icur) / 8;
+            iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+            if (icur != json[attr]) {
+                bstop=false;
+            }
+            if(attr=="opacity"){
+                obj.style.filter='alpha(opacity:'+(icur+iSpeed)+')';
+                obj.style.opacity=(icur+iSpeed)/100;
+            }else{
+                obj.style[attr]= icur + iSpeed+"px";
+            }
+        }
+        if(bstop){
+            if(fn){
+                fn();
+            }
+            clearInterval(timer);
+        }
+    }, 30)
+}
